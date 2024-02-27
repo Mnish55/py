328c46d482a2c78b65005a66a88b9003f26454be
Mnish55 <manishkumarchaudhary90@gmail.com>
2024-02-27 13:51:36 +0530

    file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="manish_calculator",
+  version="0.1.0",
+  author="Your Name",
+  author_email="your_email@example.com",
+  description="A simple calculator package",
+  packages=find_packages(),
+  install_requires=[],
+)
